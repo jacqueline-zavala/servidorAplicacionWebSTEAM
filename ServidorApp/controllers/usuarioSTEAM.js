45f7040281f9a18efa66a2498a09f142feb20356
A01750170 <A01750170@itesm.mx>
2021-04-20 18:26:34 -0500

    Controlador para iniciar sesion UsuarioSTEAM

--- ServidorApp/controllers/usuarioSTEAM.js
+++ ServidorApp/controllers/usuarioSTEAM.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const STEAM = require('../util/database').models.STEAM;
-
+const { encrypt, decrypt } = require('../util/crypto');
 
 exports.postRegistroUsuarioSTEAM = (req, res) => {
+    var hashedPassword = encrypt(req.body.password);
+    var pass = hashedPassword.iv+ '|' + hashedPassword.content
     STEAM.create({
         correoElectronico: req.body.correoElectronico,
-        contrasena: req.body.contrasena,
+        contrasena: pass,
         nombre: req.body.nombre,
         apellidoPaterno: req.body.apellidoPaterno,
         apellidoMaterno: req.body.apellidoMaterno
@@ -16,3 +18,17 @@ exports.postRegistroUsuarioSTEAM = (req, res) => {
         res.send("error")
     })
 };
+
+exports.postIniciarSesion = (req,res) => {
+        STEAM.findByPk(req.body.username)
+        .then(usuario => {
+            var hashedPassword = encrypt(req.body.password);
+            var pass = hashedPassword.iv+ '|' + hashedPassword.content
+            if(jugador.username == req.body.username && jugador.password == pass){
+                res.send('success');
+            }
+            else{
+                res.send('failed');
+            }
+        })
+}
