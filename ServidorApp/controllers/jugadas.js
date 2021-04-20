19212c128ac844c2c0f124e1818630f5f67aa331
Erick Bustos <A01378966@itesm.mx>
2021-04-20 17:30:41 -0500

    Controladores Nuevos

---
+++ ServidorApp/controllers/jugadas.js
@@ -0,0 +1,2 @@
+const path = require('path');
+const Jugadas = require('../util/database').models.Jugadas;
---
+++ ServidorApp/controllers/partida.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const Partida = require('../util/database').models.Partida;
+
+
+exports.postCrearPartida = (req, res) => {
+
+};
