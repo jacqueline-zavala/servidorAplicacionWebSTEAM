05a557f119da73d791dc50719bcbd9798d2b7f4e
A01750170 <A01750170@itesm.mx>
2021-04-27 19:40:36 -0500

    Query corregido
    Co-Authored-By: jacqueline-zavala <73703545+jacqueline-zavala@users.noreply.github.com>

--- ServidorApp/controllers/partida.js
+++ ServidorApp/controllers/partida.js
@@ -58,7 +58,7 @@ exports.postFinalizarPartida = (req,res) => {
 
 //Envia el TOP 10 de mejores puntuaciones
 exports.getMejoresPuntuaciones = (req,res) => {
-    sequelize.query("SELECT TOP (10) JugadorUsername, puntuacionAcumulada FROM Partida")
+    sequelize.query("SELECT TOP (10) JugadorUsername, puntuacionAcumulada FROM Partida WHERE puntuacionAcumulada >0")
     .then(resultado => {
         console.log(resultado);
         res.send(resultado);
